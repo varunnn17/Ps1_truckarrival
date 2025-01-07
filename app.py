8cbb0ff9e5b0a57abc9a609f9fd66365748c78c1
Varun Gopal <varun.gopal@newcold.com>
2025-01-07 11:14:47 +0530

    added the download link to csv for render phase3

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ import pickle
 import gzip
 import importlib
 
-#from feature_engineering import preprocess_and_engineer_features  # Import original feature engineering function
+from feature_engineering import preprocess_and_engineer_features  # Import original feature engineering function
 
 app = Flask(__name__)
 
@@ -134,7 +134,7 @@ try:
 except Exception as e:
     print(f"Error loading model: {e}")
 
-feature_engineering = importlib.import_module('feature_engineering')
+# feature_engineering = importlib.import_module('feature_engineering')
 # Load the feature engineering function under a new name
 try:
     with open('models/preprocessing_pipeline.pkl', 'rb') as f:
